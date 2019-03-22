4cbbf362e2b296ebda471768f7fef3675c85636f
Ivan Driuk <idriuk@zaraffasoft.com>
2019-03-22 11:57:21 +0200

    greetings with flex and justify content

--- App.js
+++ App.js
@@ -9,14 +9,26 @@
 import React, {Component} from 'react';
 import {Text, View, Image} from 'react-native';
 
+class Greeting extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1
+      }}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
     );
   }
 }
