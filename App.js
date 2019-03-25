3c26bf3e44cf1dbafce231fd5936094238244394
Ivan Driuk <idriuk@zaraffasoft.com>
2019-03-25 10:52:32 +0200

    align items

--- App.js
+++ App.js
@@ -13,11 +13,18 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      // Try setting `alignItems` to 'flex-start'
+      // Try setting `justifyContent` to `flex-end`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+      }}>
+        <View style={{height: 50, width: 50, backgroundColor: 'powderblue'}} />
+        <View style={{height: 50, width: 50, backgroundColor: 'skyblue'}} />
+        <View style={{height: 100, width: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
